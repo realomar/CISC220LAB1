d05cb6c4e4dde5f122f0cba909f3bf90aa578e91
Omar Ahmad <eatmorerice@outlook.com>
2018-08-30 15:04:34 -0400

    problem 1

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,10 +1,22 @@
 /*
- * LAB1.cpp
- *
+ * CISC220 LAB 1
  *  Created on: Aug 30, 2018
- *      Author: eatmo
+ *      Authors: omaromar@udel.edu Omar Ahmad
+ *      		 tamimoz@udel.edu  Ahmad Tamimi
  */
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+int stars(); //problem 2
 
+int main(){
+	cout << "Hello world" << endl;
+	return 0;
+}
 
+int stars(){
+	cout << "*****"<< endl;
+	return null;
+}
 
