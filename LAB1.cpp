29babf1d4f031af63a1bc3132b90d432d2dbd610
Omar Ahmad <eatmorerice@outlook.com>
2018-08-30 15:16:26 -0400

    part of part 3

--- LAB1.cpp
+++ LAB1.cpp
@@ -8,15 +8,25 @@
 #include <stdlib.h>
 using namespace std;
 
-int stars(); //problem 2
+int stars(); // problem 2
+bool isPrime(int a); // problem3
 
-int main(){
+int main(){ // problem 1
 	cout << "Hello world" << endl;
 	return 0;
 }
 
 int stars(){
-	cout << "*****"<< endl;
-	return null;
+	cout << "*****" << endl;
+	return 0;
 }
 
+bool isPrime(int a){
+	if(a < 1) return False;
+	else{
+		int div = a - 1;
+		while(div != 1){
+			if (a%div  == 0) // finish this later
+		}
+	}
+}
